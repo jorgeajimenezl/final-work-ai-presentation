350d3c008d890794103639d2535274bcf3e31f14
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2022-11-14 00:51:18 -0500

    Added more specific pixel explanation in unet

--- scenes/unet.py
+++ scenes/unet.py
@@ -69,11 +69,12 @@ class UNet(Scene):
         self.pause()
 
         pixels = VGroup()
-        for _ in range(24 * 32):
+        for i in range(32 * 24):
             square = Square(
                 stroke_width=1.0,
             )
             pixels.add(square)
+
         pixels.arrange_in_grid(24, 32, buff=0)
         pixels.rescale_to_fit(image[1].length_over_dim(0), dim=0, stretch=False)
         pixels.rescale_to_fit(image[1].length_over_dim(1), dim=1, stretch=False)
@@ -84,6 +85,7 @@ class UNet(Scene):
         mask_cmap = get_mask(mask_id, "hsv")
 
         p = 0
+        pixel_map = dict()
         for y in range(24):
             for x in range(32):
                 square = pixels[p]
@@ -97,6 +99,8 @@ class UNet(Scene):
                 num.set_color(rgb_to_color(rgb / 255))
                 num.move_to(square)
                 numbers.add(num)
+
+                pixel_map[(y, x)] = (square, num)
 
         pixels.sort(lambda p: np.dot(p, DOWN + RIGHT))
 
@@ -114,6 +118,58 @@ class UNet(Scene):
         self.play(Write(numbers))
         self.pause()
 
+        pixel_index = (12, 19)
+
+        self.play(
+            *[
+                FadeOut(pixel)
+                for index, (pixel, _) in pixel_map.items()
+                if index != pixel_index
+            ],
+            *[
+                FadeOut(num)
+                for index, (_, num) in pixel_map.items()
+                if index != pixel_index
+            ],
+        )
+
+        # Save state of (image, pixel square, pixel number)
+        tmp_group = Group(image, pixel_map[pixel_index][0], pixel_map[pixel_index][1])
+        pos = tmp_group.get_center()
+        self.play(tmp_group.animate.scale(0.5).move_to(LEFT * 3))
+
+        # Write classes
+        classes = (
+            VGroup(
+                Text("..."),
+                Text("[9] Tree", color=YELLOW),
+                Text("[10] Automobile", color=GREEN),
+                Text("[11] Traffic Light", color=RED),
+                Text("..."),
+            )
+            .arrange(DOWN, buff=MED_LARGE_BUFF)
+            .scale(0.5)
+            .move_to(RIGHT * 3)
+        )
+        tmp_box = SurroundingRectangle(classes[2])
+        arrow = Arrow(
+            start=pixel_map[pixel_index][0].get_right(),
+            end=tmp_box.get_left(),
+            stroke_width=3,
+        )
+
+        self.play(Write(classes), Create(tmp_box), Create(arrow))
+
+        self.pause()
+        self.play(
+            FadeOut(classes),
+            FadeOut(tmp_box),
+            FadeOut(arrow),
+            tmp_group.animate.scale(2).move_to(pos),
+        )
+
+        # Go back to restore everything
+        self.wait()
         self.play(
             FadeOut(numbers),
             FadeOut(pixels),
@@ -121,14 +177,16 @@ class UNet(Scene):
         )
 
         mask_vect = get_mask(read_image("resources/segm-mask.png")[:, :, 0], "hsv")
+        mask_image = ImageMobject(mask_vect).scale(1.8).shift(DOWN * 0.5)
         self.play(
             Transform(
                 image[1],
-                ImageMobject(mask_vect).scale(1.8).shift(DOWN * 0.5),
+                mask_image,
                 replace_mobject_with_target_in_scene=True,
             ),
         )
         self.pause()
+        self.remove(mask_image)
         self.play(FadeOut(image))
 
         self.play(
